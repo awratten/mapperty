cc7609526a1e9fce0391b282f626c53bc2c2e7cc
Anthony Wratten <awratten1@gmail.com>
2017-05-30 19:42:20 +1000

    Update index.php

--- index.php
+++ index.php
@@ -2,25 +2,33 @@
   <head>
   <title>Mapperty - Easy Property Listing Website</title>
   
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
 
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
-  <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
-
+    
     <style>
       #map { height: 800px;}
     </style>
   </head>
   <body>
 
-    <div id="map"></div>
+    <div id='map' style='width: 400px; height: 300px;'></div>
 
-    
-    
     <script>
-    var map = L.map('map', {
-    center: [51.505, -0.09],
-    zoom: 13
-    });
+    
+      var map = L.map('map').setView([51.505, -0.09], 13);
+
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.light',
+        accessToken: 'sk.eyJ1IjoiYXdyYXR0ZW4iLCJhIjoiY2ozYmRhanNoMDFoaTJ3cGg5MWx2MWpkeCJ9.-GFKfnIKHDY83-nD8LLi9g'
+      }).addTo(map);
+
     </script>
   </body>
 </html>
