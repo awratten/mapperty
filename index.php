407c44ffd72d42b8f07f3baffd2b4800f33b58fd
Anthony Wratten <awratten1@gmail.com>
2017-05-30 19:26:22 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <html>
-<head>
-<title>Mapperty - Easy Property Listing Website</title>
+  <head>
+  <title>Mapperty - Easy Property Listing Website</title>
+  
 
-</head>
-<body>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
+  <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
 
+    <style>
+      #map { height: 800px;}
+    </style>
+  </head>
+  <body>
 
+    <div id="map"></div>
 
-</body>
+    
+    
+    <script>
+    var map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 13
+    });
+    </script>
+  </body>
 </html>
