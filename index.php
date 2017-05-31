9e78fa54ac57ad892336e978e1704161007ae6bc
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-05-31 15:30:08 +1000

    add links to websites

--- index.php
+++ index.php
@@ -117,11 +117,11 @@
 //});
 
             var m_cluster = L.markerClusterGroup({ chunkedLoading: true });
-                m_cluster.addLayer(L.marker([-16.83142, 145.69413], {icon: AP_Logo}).addTo(map).bindPopup("Ainslie Place")); //Ainslie Place
-                m_cluster.addLayer(L.marker([-17.04103, 145.73976], {icon: MP_Logo}).addTo(map).bindPopup("Mount Peter")); //Mount Peter
-                m_cluster.addLayer(L.marker([-16.7963, 145.7083], {icon: HMB_Logo}).addTo(map).bindPopup("Half Moon Bay")); //Half Moon Bay
-                m_cluster.addLayer(L.marker([-16.98674, 145.73359], {icon: RE_Logo}).addTo(map).bindPopup("Rainforest Edge")); //Rainforest Edge
-                m_cluster.addLayer(L.marker([-16.98912, 145.74466], {icon: SA_Logo}).addTo(map).bindPopup("Sheldon Avenue")); //Sheldon Avenue
+                m_cluster.addLayer(L.marker([-16.83142, 145.69413], {icon: AP_Logo}).addTo(map).bindPopup("<a href="http://kenfrosthomes.com.au/ainslie-place-north-point/">More info...</a>")); //Ainslie Place
+                m_cluster.addLayer(L.marker([-17.04103, 145.73976], {icon: MP_Logo}).addTo(map).bindPopup("<a href="http://mtpeterestate.com.au">More info...</a>")); //Mount Peter
+                m_cluster.addLayer(L.marker([-16.7963, 145.7083], {icon: HMB_Logo}).addTo(map).bindPopup("<a href="http://kenfrosthomes.com.au/half-moon-bay/">More info...</a>")); //Half Moon Bay
+                m_cluster.addLayer(L.marker([-16.98674, 145.73359], {icon: RE_Logo}).addTo(map).bindPopup("<a href="http://rainforest-edge.com.au">More info...</a>")); //Rainforest Edge
+                m_cluster.addLayer(L.marker([-16.98912, 145.74466], {icon: SA_Logo}).addTo(map).bindPopup("<a href="http://sheldonavenue.com.au">More info...</a>")); //Sheldon Avenue
                 //m_cluster.addLayer(L.marker([-16.8186,145.7781], {icon: defaultMarker}).addTo(map).bindPopup("I am a green leaf."));
             map.addLayer(m_cluster);
             
