3e47d5cc89a76d651f708ee0a2fd741069c7835f
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-06-01 10:02:54 +1000

    fix add search

--- index.php
+++ index.php
@@ -80,6 +80,10 @@
 
             var map = L.map('map').setView([-16.98720, 145.74003] , 10);
 
+            var geoSearchController = new L.Control.GeoSearch({
+                provider: new L.GeoSearch.Provider.Google()
+            }).addTo(map);
+
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
                 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
                 maxZoom: 18,
