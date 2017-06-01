3ccd3e7424199918abc2c9b7fa441ff503e06208
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-06-01 11:47:04 +1000

    add google tracking code

--- index.php
+++ index.php
@@ -59,6 +59,16 @@
         </style>
     </head>
     <body>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-100281918-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+        
         <div id='map' class="container-fluid"></div>
         <?php if (filter_input(INPUT_GET, 'logo', FILTER_SANITIZE_STRING) == 'true') { echo "<div id='logo'></div>";}?>
         <?php if (filter_input(INPUT_GET, 'search', FILTER_SANITIZE_STRING) == 'true') { echo "<script src='js/geoSearch_include.js' type='text/javascript'></script>"; }?>
