6826fd2b2c80bc1f436d64dc1c7011f1a08f5ee8
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-06-06 11:29:38 +1000

    add new properties

--- thinkvr/js/PropertyList.js
+++ thinkvr/js/PropertyList.js
@@ -22,6 +22,13 @@ var addressPoints = [
     [-16.9255144,145.77569340000002, "<a href='https://www.thinkvirtualreality.com.au/tour/citi-central-offices-3d-vr-tour/fullscreen/' target='_blank'><button class='btn'>Visit Website</button></a>", Icon_Logo],
     [-16.9275,145.777778, "<a href='https://www.thinkvirtualreality.com.au/tour/cairns-convention-centre-meeting-rooms-1-7-3d-vr-tour/fullscreen/' target='_blank'><button class='btn'>Visit Website</button></a>", Icon_Logo],
     [-16.9009497,145.74283779999996, "<a href='https://www.thinkvirtualreality.com.au/tour/137-collins-avenue-edge-hill-qld-4870/fullscreen/' target='_blank'><button class='btn'>Visit Website</button></a>", Icon_Logo],
+    [-16.914489,145.75140250000004, "<a href='https://www.thinkvirtualreality.com.au/tour/39-anderson-street-manunda-qld-4870/fullscreen/' target='_blank'><button class='btn'>Visit Website</button></a>", Icon_Logo],
+    [-16.8987692,145.73216649999995, "<a href='https://www.thinkvirtualreality.com.au/tour/47-49-heavey-cres-whitfield/fullscreen/' target='_blank'><button class='btn'>Visit Website</button></a>", Icon_Logo],
+    [-16.9524038,145.7498428, "<a href='https://www.thinkvirtualreality.com.au/tour/18-spotto-close-woree-qld-4868/fullscreen/' target='_blank'><button class='btn'>Visit Website</button></a>", Icon_Logo],
+    
+ 
+
+
 ];
 
 
