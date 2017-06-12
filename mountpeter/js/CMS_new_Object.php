48d385b2847bedd09bd71396f0953c0871e4c04a
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-06-12 15:52:36 +1000

    update file

--- mountpeter/js/CMS_new_Object.php
+++ mountpeter/js/CMS_new_Object.php
@@ -16,12 +16,17 @@ and open the template in the editor.
         ?>
         
 <?php echo "var L334 = L.polygon("; ?>
+    <?php echo "<br>";?>
 <?php echo "[[-17.04440377800294,145.7398823900024],[-17.04433972381106,145.7400585369476],[-17.04408661553813,145.7399586814424],[-17.04413759740198,145.7398178552146],[-17.04418422098722,145.7397957517161],[-17.04440377800294,145.7398823900024]],"; ?>
+        <?php echo "<br>";?>
 <?php echo "{className: 'LotStyle'}"; ?>
 <?php echo ").bindPopup('<strong>Lot 334 Homevale Entrance</strong>');"; ?>
+        <?php echo "<br>";?>
 <?php echo "LotStage3.addLayer(L334);";?>
+        <?php echo "<br>";?>
 <?php echo "LotStage_Circles.addLayer( new L.circle( L334.getBounds().getCenter() , { className: 'LotCircle' , radius: C_Radius}));";?>
 
 
     </body>
 </html>
+<a href="CMS_new_Object.php"></a>
