481e58256d114e0cc0134a084dfdcde0edeeb36f
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-07-27 12:40:52 +1000

    add python REAXML passer

---
+++ mountpeter/mapperty_rex.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'mtpeterestate.com.au/XML/feedsync/?action=do_output&type=residential'
+r  = requests.get("http://" +url)
+data = r.text
+soup = BeautifulSoup(data, 'lxml')
+
+
+for prop in soup.find_all('residential'):
+	status = prop.get('status')
+	streetnumber = (prop.find_all('streetnumber', text=True)[0].contents[0])
+	streetName = (prop.find_all('street', text=True)[0].contents[0])
+
+	address_string = (streetnumber + " " + streetName)
+
+	priceView = (prop.find_all('priceview', text=True)[0].contents[0])
+
+	Lot_Number = streetnumber.split()[-1]
+
+	uniqueID = ("L" + Lot_Number)
+
+	if status == "current":
+		status = "LotCircle"
+	elif status == "sold":
+		status = "LotCircle_SOLD"
+
+	search_URL = 'https://mtpeterestate.com.au/property/?s=' + address_string.replace(' ', '+')
+
+	print ("var " + uniqueID + " = L.polygon([")
+	print ("")
+	print ("], {className: 'LotStyle'}).bindPopup('<center><strong>" + address_string + "</strong><br><p>Price: " + priceView + "</p><br><a href='" + search_URL + "' target='_blank'><br><button class='btn btn-success'>More Info...</button></a></center>');")
+	print ("LotStage1.addLayer(" + uniqueID + ");")
+
+	print ("LotStage_Circles.addLayer(new L.circle(" + uniqueID + ".getBounds().getCenter(), {className: '" + status + "', radius: C_Radius}));")
+
+	print("")
+
+
