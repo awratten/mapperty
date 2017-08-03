ae4e6fd906935482d0cbbd1616a63a4e3a581bc0
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-08-03 10:27:59 +1000

    update

--- floorplan/index.php
+++ floorplan/index.php
@@ -89,7 +89,7 @@
 
         <div id='map' class="container-fluid"></div>
         
-        <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="VRModalLabel" aria-hidden="true">
+        <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
